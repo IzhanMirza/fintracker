0f2c7d537aed56b8ea35e1035e6825fb0d030065
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-10-30 20:57:59 +0530

    [UI UPDATE]

--- lib/screens/settings/settings.screen.dart
+++ lib/screens/settings/settings.screen.dart
@@ -8,7 +8,6 @@ import 'package:fintracker/widgets/dialog/confirm.modal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:settings_ui/settings_ui.dart';
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
